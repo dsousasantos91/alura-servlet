595f48bb31abdf211d982cc436174740898a0c7b
Douglas de Sousa Santos <dsousasantos91@gmail.com>
2020-01-26 15:21:28 -0300

    Concluir Aula VII - Modulo II

--- cliente-webservice/src/br/alura/cliente/ClienteWebService.java
+++ cliente-webservice/src/br/alura/cliente/ClienteWebService.java
@@ -7,7 +7,7 @@ public class ClienteWebService {
 	public static void main(String[] args) throws Exception {
 
 		String conteudo = Request.Post("http://localhost:8080/gerenciador/empresas")
-				.addHeader("Accept", "application/xml")
+				.addHeader("Accept", "application/json")
 				.execute()
 				.returnContent()
 				.asString();
